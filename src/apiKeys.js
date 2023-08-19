6a784c5c6a7ba299d459ea9f282ce4afe3edc6fb
Dinesh Kumar <dk9671399889@gmail.com>
2023-08-19 16:17:06 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "5e89496f65520beec778ec6be8a23985",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
